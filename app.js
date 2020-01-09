8643a8d524371992d63c0ce8a2253718c2cbc145
rightbrainpapi <dahuemaen@gmail.com>
2020-01-09 16:47:12 -0500

    adding a module setup

--- app.js
+++ app.js
@@ -5,3 +5,24 @@
 // add the new item to the UI
 // calculate the new budget
 // Update the UI
+
+
+//  Iffy -  Immediately invoked function or expression
+// returns and object containing all of the functions e want public
+var budgetController = (function(){
+
+var x = 23;
+
+var add = function(a){
+    return x + a;
+}
+
+return {
+    publicTest: function(b){
+        console.log(add(b));
+    }
+}
+})();
+
+
+console.log("This is the page.")
