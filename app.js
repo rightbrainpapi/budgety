97d34e4fdfcc52bdc31446217999a9a1514f80db
rightbrainpapi <dahuemaen@gmail.com>
2020-01-09 16:18:34 -0500

    planning steps and breaking steps down into modules

---
+++ app.js
@@ -0,0 +1,7 @@
+// add event handler
+// get input values
+// add the new item to our data structure
+// add the new item to the UI
+// add the new item to the UI
+// calculate the new budget
+// Update the UI
