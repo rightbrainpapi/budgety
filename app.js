6ee6f16d41b94ffb9d218d9022e32c098e9c23e7
rightbrainpapi <dahuemaen@gmail.com>
2020-01-10 06:46:54 -0500

    created ability to add new item to budget controller

--- app.js
+++ app.js
@@ -35,7 +35,40 @@ var budgetController = (function(){
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    // Public Method - To allow other modules to add new item into data structure
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // Create new item based on "inc" or "exp" type
+            if (type === "exp"){
+                newItem = new Expense (ID, des, val);
+            }
+            else if (type === "inc"){
+                newItem = new Income (ID, des, val);
+            }
+
+            // Pushes new item into the proper array depending on the type
+            data.allItems[type].push(newItem)
+
+            // return the new elements
+            return newItem;
+              
+        }
+        // Used this to see whether budget controller had acess to the data
+        // testing: function() {
+        //     console.log(data);
+        // }
+    };
 
 
 })();
@@ -88,10 +121,12 @@ var controller = (function(budgetCtrl, UICtrl){
     };
 
     var ctrlAddItem = function (){
+        var input, newItem;
         // [x] get the field input 
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
+        
         // [] add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value ); // We are getting the type, deescription and value from the UICtrl,getInput above
         // [] Add the item to the UI
         // [] calculate the budget
         // [] display the budget on the UI
