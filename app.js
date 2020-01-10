76bf94b47158ac2d91b99281ea045ccde7bf345d
rightbrainpapi <dahuemaen@gmail.com>
2020-01-10 04:16:59 -0500

    about to begin reading input data

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ var controller = (function(budgetCtrl, UICtrl){
     document.addEventListener('keypress', function(event){
         if (event.keyCode === 13 || event.which === 13){
             ctrlAddItem();
+            
         }
     })
 
